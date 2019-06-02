4f5b2bcab8e27cc30b3a9dfcc452f9af38dd129a
pacifistes <bbrunell@student.42.fr>
2019-06-02 16:10:21 +0200

    rejout des ressources du sujet

--- includes/scop_math.h
+++ includes/scop_math.h
@@ -6,7 +6,7 @@
 /*   By: bbrunell <bbrunell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/10 21:29:29 by bbrunell          #+#    #+#             */
-/*   Updated: 2019/05/23 18:36:47 by bbrunell         ###   ########.fr       */
+/*   Updated: 2019/06/02 15:52:10 by bbrunell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,29 +21,31 @@
 # define DEG_TO_RAD (PI / 180.0)
 
 
-typedef struct s_vector
+typedef struct	s_vector
 {
 	float x;
 	float y;
 	float z;
 }				t_vector;
 
-void		rotate_x(float matrice[16], float angle);
-void		rotate_y(float matrice[16], float angle);
-void		rotate_z(float matrice[16], float angle);
-void		rotate_with_axis(float matrice[16], float angle, t_vector axis);
+t_vector		normalize(t_vector v);
+t_vector		cross(t_vector v1, t_vector v2);
+float			dot(t_vector v1, t_vector v2);
+void			init_matrice(float matrice[16], float value);
+void			look_at(float matrice[16], t_vector eye, t_vector center,
+t_vector up);
+t_vector		new_vector(float x, float y, float z);
+void			perspective(float matrice[16], float fov, float aspect);
+void			perspective2(float matrice[16], float fov, float aspect,
+float width, float height);
+void			rotate_with_axis(float matrice[16], float angle, t_vector axis);
+void			rotate_x(float matrice[16], float angle);
+void			rotate_y(float matrice[16], float angle);
+void			rotate_z(float matrice[16], float angle);
+t_vector		scale(t_vector v1, float value);
+void			translate(float matrice[16], t_vector vector);
+t_vector		vec_sub(t_vector v1, t_vector v2);
+t_vector		vec_add(t_vector v1, t_vector v2);
 
-float		dot(t_vector v1, t_vector v2);
-t_vector	normalize(t_vector v);
-t_vector	vec_sub(t_vector v1, t_vector v2);
-t_vector	vec_add(t_vector v1, t_vector v2);
-t_vector	new_vector(float x, float y, float z);
-t_vector	cross(t_vector v1, t_vector v2);
-t_vector	scale(t_vector v1, float value);
 
-void	init_matrice(float matrice[16], float value);
-void	perspective(float matrice[16], float fov, float aspect);
-void	perspective2(float matrice[16], float fov, float aspect, float width, float height);
-void	translate(float matrice[16], t_vector vector);
-void	look_at(float matrice[16], t_vector eye, t_vector center, t_vector up);
 #endif
